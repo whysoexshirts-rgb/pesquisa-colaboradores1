7e474068a38d5ef4ea2c9fea484cef92813d81ab
whysoexshirts-rgb <whysoexshirts@gmail.com>
2026-07-24 10:50:21 +0000

    Adicionar suporte PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/icon.svg'],
+      manifest: {
+        name: 'SC Finder',
+        short_name: 'SC Finder',
+        description: 'Pesquisa de colaboradores',
+        theme_color: '#0B7A3B',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 })
